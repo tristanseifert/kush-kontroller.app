3959cab1b762b1d9a49ee8757857b336245c35cf
Tristan Seifert <tristan@tseifert.me>
2022-10-14 14:43:48 -0700

    initial commit

---
+++ Kush Kontroller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kush Kontroller
+//
+//  Created by Tristan Seifert on 20221014.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# Kush Kontroller
+Basic iOS app to control various Bluetooth-connected vaporizers.
+
+## Device Support
+The following devices are supported:
+
+- Pax Labs
+    - Pax 3
+    - Pax Era (non-Pro, micro USB model)
+
+### Disclaimers
+Pax 3, Pax Era and Pax Era Pro are trademarks of Pax Labs.
