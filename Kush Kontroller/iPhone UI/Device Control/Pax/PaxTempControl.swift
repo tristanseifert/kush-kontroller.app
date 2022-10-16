08ef81a0141930d263e4683825bc367663051bb9
Tristan Seifert <tristan@tseifert.me>
2022-10-16 12:19:06 -0700

    Layer-based temperature control wheel
    - Start working on a temperature control that's rendered using layers
      rather than manually drawn (for the good conic gradient goodness)

--- Kush Kontroller/iPhone UI/Device Control/Pax/PaxTempControl.swift
+++ Kush Kontroller/iPhone UI/Device Control/Pax/PaxTempControl.swift
@@ -10,6 +10,312 @@
 import Foundation
 import UIKit
 
+/**
+ * @brief Control for changing temperature
+ *
+ * This is a circular slider dude
+ */
+public class PaxTempControl2: UIControl {
+    /// Color of the knob
+    @IBInspectable public var knobColor: UIColor = .gray  {
+        didSet {
+            self.updateLayerColors()
+            self.setNeedsDisplay()
+        }
+    }
+    
+    /// Track border width
+    @IBInspectable public var borderWidth: CGFloat = 3 {
+        didSet {
+            self.updateLayerFrames()
+            self.setNeedsDisplay()
+        }
+    }
+    /// Track border color
+    @IBInspectable public var borderColor: UIColor = .white {
+        didSet {
+            self.updateLayerColors()
+            self.setNeedsDisplay()
+        }
+    }
+    
+    /// Size of the knob (pt)
+    @IBInspectable public var knobWidth: CGFloat = 45 {
+        didSet {
+            self.updateLayerFrames()
+            self.setNeedsDisplay()
+        }
+    }
+    /// Width of the track (pt)
+    @IBInspectable public var trackWidth: CGFloat = 30 {
+        didSet {
+            self.updateLayerFrames()
+            self.setNeedsDisplay()
+        }
+    }
+    /// Font size for the value
+    @IBInspectable public var fontSize: CGFloat = 52 {
+        didSet {
+            self.updateLayerFont()
+            self.updateLayerText()
+            self.setNeedsDisplay()
+        }
+    }
+    
+    /// Minimum temperature (deg °C)
+    @IBInspectable public var minValue: CGFloat = 175
+    /// Maximum value (deg °C)
+    @IBInspectable public var maxValue: CGFloat = 215
+    /// Actual value (deg °C)
+    @IBInspectable public var value: CGFloat = 175 {
+        didSet {
+            self.updateLayerText()
+            self.setNeedsDisplay()
+        }
+    }
+    /// Unit to format for display
+    @IBInspectable public var unit: UnitTemperature = .celsius {
+        didSet {
+            self.updateLayerText()
+            self.setNeedsDisplay()
+        }
+    }
+    
+    // MARK: Initialization
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initLayers()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.initLayers()
+    }
+    
+    // MARK: - Control stuff
+    // MARK: Interface Builder support
+    /**
+     * @brief Interface Builder support method
+     *
+     * Force re-drawing and layer updates in IB
+     */
+    public override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        
+        self.updateLayerFont()
+        self.updateLayerColors()
+        self.updateLayerFrames()
+        self.setNeedsDisplay()
+        self.layer.setNeedsDisplay()
+    }
+    
+    // MARK: View events
+    /**
+     * @brief Color scheme has changed
+     *
+     * Used to update the colors on all layers to match the current color scheme.
+     */
+    public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        self.updateLayerColors()
+        self.setNeedsDisplay()
+    }
+    
+    // MARK: Control events
+    
+    // MARK: - Layer management
+    /// KVO dude on the bounds
+    private var boundsObserver: Any?
+    
+    /// Bezier path representing the slider's circular track
+    private var trackPath: UIBezierPath!
+    
+    /// Background gradient
+    private var trackGradient: CAGradientLayer!
+    /// Mask for background gradient
+    private var trackMask: CALayer!
+    /// Outline for track
+    private var trackOutline: CAShapeLayer!
+    /// Layer for current value
+    private var valueLabel: CATextLayer!
+    
+    /**
+     * @brief Perform common initialization
+     *
+     * This creates the various required layers.
+     */
+    private func initLayers() {
+        // prepare root view layer with layout manager
+        self.layer.masksToBounds = true
+        
+        // gradient layer for track
+        self.trackGradient = CAGradientLayer()
+        self.trackGradient.startPoint = CGPoint(x: 0.33, y: 0.95)
+        self.trackGradient.endPoint = CGPoint(x: 1, y: 0.95)
+        self.trackGradient.type = .conic
+        self.trackGradient.colors = [
+            UIColor.white.cgColor,
+            UIColor.blue.cgColor, UIColor.green.cgColor,
+            UIColor.yellow.cgColor, UIColor.red.cgColor
+        ]
+        self.trackGradient.locations = [0, 0.4, 0.5, 0.75, 1]
+        
+        self.layer.addSublayer(self.trackGradient)
+        
+        // mask for gradient layer
+        self.trackMask = CALayer()
+        self.trackGradient.mask = self.trackMask
+        
+        // track outline
+        self.trackOutline = CAShapeLayer()
+        self.trackOutline.lineCap = .round
+        self.trackOutline.lineJoin = .round
+        self.layer.addSublayer(self.trackOutline)
+        
+        // knob
+        
+        // current value label
+        self.valueLabel = CATextLayer()
+        self.valueLabel.alignmentMode = .center
+        self.valueLabel.masksToBounds = false
+        
+        self.valueLabel.allowsFontSubpixelQuantization = true
+        self.valueLabel.contentsScale = self.window?.screen.scale ?? 3
+        
+        self.layer.addSublayer(self.valueLabel)
+        
+        // add an observer on the bounds
+        self.boundsObserver = self.observe(\PaxTempControl2.bounds,
+                                           options: [.new], changeHandler: { view, change in
+            self.updateLayerFrames()
+        })
+        
+        // initial configuration
+        self.updateLayerColors()
+        self.updateLayerFont()
+        self.updateLayerText()
+        
+        self.updateLayerFrames()
+    }
+    
+    /**
+     * @brief Update colors of all layers
+     *
+     * Since CGColorRef's don't follow the system color scheme, we need to manually do this
+     * whenever the color scheme changes.
+     */
+    private func updateLayerColors() {
+        self.trackOutline.strokeColor = self.borderColor.cgColor
+        self.valueLabel.foregroundColor = UIColor.label.cgColor
+    }
+    
+    /**
+     * @brief Update the font of the text label
+     */
+    private func updateLayerFont() {
+        let font = UIFont.monospacedDigitSystemFont(ofSize: self.fontSize,
+                                                    weight: .heavy)
+        self.valueLabel.font = nil
+        self.valueLabel.font = font
+        self.valueLabel.fontSize = font.pointSize
+        
+        // the font size changed, so the frame needs updating
+        self.valueLabel.frame = CGRect(origin:CGPoint(x: 0, y: CGRectGetMidY(self.bounds) - (font.lineHeight / 2)),
+                                       size: CGSize(width: self.bounds.width, height: font.lineHeight))
+    }
+    
+    /**
+     * @brief Update the text label contents
+     */
+    private func updateLayerText() {
+        let value = Measurement(value: self.value, unit: UnitTemperature.celsius).converted(to: self.unit)
+        let formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .medium
+        formatter.numberFormatter.maximumFractionDigits = 0
+        self.valueLabel.string = formatter.string(from: value)
+    }
+    
+    // MARK: Bounds
+    /**
+     * @brief Update frames of layers
+     */
+    private func updateLayerFrames() {
+        let bounds = self.bounds
+        
+        // update layers
+        self.trackGradient.frame = bounds
+        self.trackMask.frame = bounds
+        
+        // update the paths of components, then redraw masks
+        self.updateTrackPath()
+        
+        self.updateMasks()
+    }
+    
+    // MARK: Paths and masks
+    /**
+     * @brief Update the track path
+     *
+     * This path represents the track of the slider, and is consumed by various parts of the
+     * slider's drawing code.
+     */
+    private func updateTrackPath() {
+        // calculate some helpful variables
+        let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+        
+        let radius = (max(bounds.width, bounds.height)) / 2
+        let knobInset = max(0, (self.knobWidth - self.trackWidth))
+        let strokeInset = max(0, self.borderWidth)
+        
+        let startAngle: CGFloat = 3 * .pi / 4
+        let endAngle: CGFloat = .pi / 4
+        
+        // set up the arc
+        self.trackPath = UIBezierPath(arcCenter: center,
+                                      radius: radius - self.trackWidth/2 - knobInset/2 - strokeInset/2,
+                                      startAngle: startAngle, endAngle: endAngle,
+                                      clockwise: true)
+        
+        // apply it to the stroke layer
+        self.trackOutline.contents = self.trackPath.cgPath
+        self.trackOutline.lineWidth = self.borderWidth
+    }
+    
+    /**
+     * @brief Draw the track mask image
+     */
+    private func updateMasks() {
+        // prepare temporary context
+        let ctxSize = self.bounds.size
+        UIGraphicsBeginImageContextWithOptions(ctxSize, false, 0)
+        let ctx = UIGraphicsGetCurrentContext()!
+        
+        // set this path as the clipping region
+        ctx.addPath(self.trackPath.cgPath)
+        
+        ctx.setLineWidth(self.trackWidth)
+        ctx.setLineCap(.round)
+        ctx.replacePathWithStrokedPath()
+        ctx.clip()
+        
+        // fill it mask
+        ctx.setFillColor(UIColor.black.cgColor)
+        ctx.fill([CGRect(origin: .zero, size: ctxSize)])
+        
+        // get image from context
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else {
+            fatalError("failed to get context image")
+        }
+        
+        self.trackMask.contents = image.cgImage
+        
+        // clean up
+        UIGraphicsEndImageContext()
+    }
+}
+
 // PRCENTAGE VIEW
 @available(iOS 15.0, *)
 @IBDesignable
