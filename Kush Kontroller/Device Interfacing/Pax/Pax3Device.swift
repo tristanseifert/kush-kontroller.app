418232e30f18ae7c731593a6264ac16a4a4b407e
Tristan Seifert <tristan@tseifert.me>
2022-10-18 16:04:29 -0700

    Add Pax Era support
    - Super basic Pax Era support, limited to setting the temperature of the
      device

--- Kush Kontroller/Device Interfacing/Pax/Pax3Device.swift
+++ Kush Kontroller/Device Interfacing/Pax/Pax3Device.swift
@@ -28,6 +28,7 @@ class Pax3Device: PaxDevice {
     // MARK: - Initialization
     override init(_ peripheral: CBPeripheral) {
         super.init(peripheral)
+        // from iOS app UI (these may not be totally correct?)
         self.validTempRange = 175.0...215.0
         self.type = .Pax3
     }
--- Kush Kontroller/Device Interfacing/Pax/PaxEraDevice.swift
+++ Kush Kontroller/Device Interfacing/Pax/PaxEraDevice.swift
@@ -13,12 +13,17 @@ import CoreBluetooth
  * Device behaviors specific to the Pax Era device
  */
 class PaxEraDevice: PaxDevice {
+    /// Temperature set point, in °C
+    @Published private(set) public var temperature: Float = Float.nan
+
     // MARK: - Initialization
     override init(_ peripheral: CBPeripheral) {
         super.init(peripheral)
+        // from iOS app UI (these may not be totally correct?)
+        self.validTempRange = 220.0...420.0
         self.type = .PaxEra
     }
-    
+
     /**
      * Fetch our additional set of attributes when the connection is set up.
      */
@@ -36,13 +41,39 @@ class PaxEraDevice: PaxDevice {
      */
     override internal func processPacket(_ packet: Data) throws {
         switch packet[0] {
+            case PaxMessageType.HeaterSetPoint.rawValue:
+                let message = try HeaterSetPointMessage(fromPacket: packet)
+                self.temperature = message.temperature
+
             // unhandled packets go to super
             default:
                 try super.processPacket(packet)
         }
     }
     
+    // MARK: Setters
+    /**
+     * Sets the desired oven set point temperature.
+     */
+    public func setTemperature(_ temp: Float) throws {
+        // TODO: remove hardcoded limits (based on iOS app)
+        guard temp >= 220, temp <= 420 else {
+            throw Errors.invalidTemperature(temp)
+        }
+
+        let message = HeaterSetPointMessage(temperature: temp)
+        try self.writePacket(message)
+
+        // ensure the value is updated
+        try self.reloadAttributes([.HeaterSetPoint])
+    }
+    
     // MARK: - Types and constants
+    enum Errors: Error {
+        /// The provided temperature value is invalid
+        case invalidTemperature(_ temperature: Float)
+    }
+
     /// Additional attributes to read from device
     private static let DefaultAttributes: Set<PaxMessageType> = [.PodInserted, .HeaterSetPoint,
                                                                  .ShellColor, .HeatingParams,
